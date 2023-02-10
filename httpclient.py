d2d56e0f1e8d2455a9a0d30be0b81404a66ab179
unknown <jche@ualberta.ca>
2023-02-10 02:47:02 -0700

    done

--- httpclient.py
+++ httpclient.py
@@ -18,6 +18,11 @@
 # Write your own HTTP GET and POST
 # The point is to understand what you have to send and get experience with it
 
+#References: 
+# https://www.cs.unb.ca/~bremner/teaching/cs2613/books/python3-doc/library/urllib.parse.html
+# https://docs.citrix.com/en-us/citrix-adc/current-release/appexpert/http-callout/http-request-response-notes-format.html
+# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers
+
 import sys
 import socket
 import re
@@ -41,13 +46,19 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        return None
+        code = int(data.split("\r\n")[0].split()[1])
+        print("code:",code)
+        return code
 
     def get_headers(self,data):
-        return None
+        header = data.split("\r\n\r\n")[0]
+        print("header:",header)
+        return header
 
     def get_body(self, data):
-        return None
+        body = data.split("\r\n\r\n")[1]
+        print("body:",body)
+        return body
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -67,17 +78,92 @@ class HTTPClient(object):
                 done = not part
         return buffer.decode('utf-8')
 
+    def manage_url(self, url):
+        parse_url = urllib.parse.urlparse(url)
+        #print(parse_url)
+        #print(parse_url.netloc)
+        
+        if ":" in parse_url.netloc:
+            host, port = parse_url.netloc.split(":")
+        else:
+            host = parse_url.netloc
+            if parse_url.scheme == "https":
+                port = 443
+            elif parse_url.scheme == "http":
+                port = 80
+        if parse_url.path == "":
+            path = "/"
+        else:
+            path = parse_url.path
+       # print(path)
+
+        return host, port, path
+
     def GET(self, url, args=None):
         code = 500
         body = ""
+        #print(url)
+        print("args--------------------------:",args)
+        host, port, path = self.manage_url(url)
+        self.connect(host, int(port))
+
+        request = "GET " + path + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" 
+        request = request + "Accept: */*\r\n"
+        request = request + "Accept-Language: en-US\r\n"
+        request = request + "Accept-Encoding: gzip, deflate\r\n"
+        request = request + "Connection: close\r\n\r\n"
+        self.sendall(request)
+
+        data = self.recvall(self.socket)
+        print("data:",data)
+        print("data type:",type(data))
+        self.close()
+
+        code = self.get_code(data)
+        header = self.get_headers(data)
+        body = self.get_body(data)
+        print("show:",code, header, body)
+
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
         code = 500
         body = ""
+        host, port, path = self.manage_url(url)
+        self.connect(host, int(port))
+
+        if args is not None:
+            Info = urllib.parse.urlencode(args)
+        else:
+            Info = ""
+        
+        request = "POST " + path + " HTTP/1.1\r\n"
+        request = request + "Host: " + host + "\r\n"
+        request = request + "Content-Type: application/x-www-form-urlencoded\r\n"
+        request = request + "Content-Length: " + str(len(Info)) + "\r\n"
+        request = request + "Accept: */*\r\n"
+        request = request + "Accept-Language: en-US\r\n"
+        request = request + "Accept-Encoding: gzip, deflate\r\n"
+        request = request + "Connection: close\r\n\r\n"
+        request = request + Info
+        self.sendall(request)
+
+        data = self.recvall(self.socket)
+        print("data:",data)
+        print("data type:",type(data))
+        self.close()
+
+        code = self.get_code(data)
+        header = self.get_headers(data)
+        body = self.get_body(data)
+        print("show:",code, header, body)
+
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
+        #print(url)
+        #print(command)
+        print("args--------------------------:",args)
         if (command == "POST"):
             return self.POST( url, args )
         else:
